90e9ea5f573011c72fea186f588fb6499a923ebd
Yash Agrawal <yagrawl2@gmail.com>
2019-08-06 21:37:30 -0700

    basic module completed

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const nmbrs = num => {
-  if(num === 1000) {
-    return '1K';
+const nmbrs = (num, params) => {
+  const num_units = {'3': 'K', '6': 'M', '9': 'B', '12': 'T'};
+
+  if(num >= 0 && num < 1000 || num < 0 && num > -1000) {
+    return num.toString();
   }
+
+  let exponent = 0;
+  let decimal_num = Math.abs(num);
+  let sign = (num >= 0) ? '' : '-';
+
+  while(decimal_num >= 10) {
+    decimal_num /= 10;
+    exponent += 1;
+  }
+
+  decimal_num = sign + decimal_num.toString();
+  num_unit = num_units[exponent.toString()];
+
+  let prettified_num = decimal_num + num_unit;
+  return prettified_num;
 }
 
 module.exports = nmbrs;
--- test.js
+++ test.js
@@ -3,6 +3,34 @@ const nmbrs = require('./index');
 
 describe('Prettify Numbers', function() {
   it('Parses number and adds appropriate unit', function(){
-    assert.equal('1K', nmbrs(1000));
+    assert.equal('1.2K', nmbrs(1200));
+    assert.equal('5.6M', nmbrs(5600000));
+    assert.equal('3.4B', nmbrs(3400000000));
+    assert.equal('8.7T', nmbrs(8700000000000));
+  });
+
+  it('Parses number less than 1000 without a unit', function(){
+    assert.equal('121', nmbrs(121));
+    assert.equal('999', nmbrs(999));
+    assert.equal('21', nmbrs(21));
+    assert.equal('1', nmbrs(1));
+  });
+
+  it('Parses 0 without a unit', function(){
+    assert.equal('0', nmbrs(0));
+  });
+
+  it('Parses number greater than -1000 without a unit', function(){
+    assert.equal('-121', nmbrs(-121));
+    assert.equal('-999', nmbrs(-999));
+    assert.equal('-21', nmbrs(-21));
+    assert.equal('-1', nmbrs(-1));
+  });
+
+  it('Parses negative numbers less than -1000 with a unit', function(){
+    assert.equal('-1.2K', nmbrs(-1200));
+    assert.equal('-5.6M', nmbrs(-5600000));
+    assert.equal('-3.4B', nmbrs(-3400000000));
+    assert.equal('-8.7T', nmbrs(-8700000000000));
   });
 });
